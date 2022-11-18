42dc61f3ffdfeb19819bec96efd7a8a02d63dafe
areebaif <areeba.iftikhar1993@gmail.com>
2022-11-18 10:50:54 -0600

    backed functions wip

--- client/src/backendFunctions.ts
+++ client/src/backendFunctions.ts
@@ -1,7 +1,27 @@
-import { PropertiesProps } from "./components/map-layout";
-
+import { PropertiesProps } from "./components/search-mapdisplay";
+// TODO: extratc out main function put it here
 export const fetchZipSearch = async (zipcodeUserInput: string) => {
   const response = await fetch(`/api/dev/${zipcodeUserInput}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  const data: {
+    data: GeoJSON.FeatureCollection<
+      GeoJSON.Geometry,
+      PropertiesProps
+    >["features"];
+  } = await response.json();
+  return data.data;
+};
+
+export const fetchStateSearch = async (stateUserInput: string) => {
+  const response = await fetch(`/api/dev/${stateUserInput}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
