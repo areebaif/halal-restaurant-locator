858a06c5c0f90890060f8a165cd4b0bffb186a24
areebaif <areeba.iftikhar1993@gmail.com>
2023-08-29 14:12:57 -0500

    fixed build errors

---
+++ pages/search/[...searchParams].tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { useRouter } from "next/router";
+
+const MapSearch: React.FC = () => {
+  const router = useRouter();
+  const urlParams = router.query;
+  return <div></div>;
+};
+
+export default MapSearch;
